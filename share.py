5303faafd4068e47291bfc71d753761615e90bcd
Tarek Ziade <tarek@ziade.org>
2014-12-23 10:30:56 +0100

    plugged a direct FxA auth to get the keys for now

--- share.py
+++ share.py
@@ -25,11 +25,6 @@ appkey = '12345'
 root = 'http://localhost:8000/'
 key_url = root + email + '/' + appname + '/key?appkey=' + appkey
 
-# secret key, we got from FxA
-kB = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
-
-# key derived for our app
-__, kBr = hkdf_expand(kB, b"SharingApp")
 
 def get_key(email, appname, appkey):
     result = requests.get(key_url)
@@ -38,7 +33,8 @@ def get_key(email, appname, appkey):
     data = result.json()
     box = nacl.secret.SecretBox(kBr)
     nonce = binascii.unhexlify(data['nonce'])
-    encrypted_private_key = nacl.utils.EncryptedMessage.fromhex(data['encPrivKey'])
+    encPrivKey = binascii.unhexlify(data['encPrivKey'])
+    encrypted_private_key = nacl.utils.EncryptedMessage(encPrivKey)
     private_key = box.decrypt(encrypted_private_key)
     private_key = PrivateKey(private_key)
     return private_key, private_key.public_key
@@ -67,14 +63,36 @@ def encrypt_key(key, secret):
     return encrypted, binascii.hexlify(nonce)
 
 
+# let's sign into FxA and grab kA, kB
+from fxa.core import Client
+from getpass import getpass
+
+client = Client("https://api.accounts.firefox.com")
+
+email = raw_input('FxA email: ')
+password = getpass('FxA password: ')
+
+print('Login into FxA')
+session = client.login(email, password, keys=True)
+print('Fetching the keys')
+kA, kB = session.fetch_keys()
+
+# kB key derived for our app
+print('Creating kBr')
+__, kBr = hkdf_expand(kB, b"SharingApp")
+
 # let's generate a public and a private key pair
+print('Generating a key/pair')
 priv, pub = generate_keys()
 pub = binascii.hexlify(pub.encode())
 enc_priv, nonce = encrypt_key(priv, kBr)
 
 # posting the key to the user directory service
+print('Posting the Key pair to the directory')
 post_key(email, appname, pub, enc_priv, nonce, appkey)
 
 # getting the key out of the user directory service
+print('Fetching the Key pair from the directory')
+
 print(get_key(email, appname, appkey))
 
