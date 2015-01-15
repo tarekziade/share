03a3900338a6298b62fecbc4f95dd25a54e5dcbe
Tarek Ziade <tarek@ziade.org>
2015-01-15 11:55:56 +0100

    first shot at stream encryption

--- fxakeys/crypto.py
+++ fxakeys/crypto.py
@@ -2,8 +2,10 @@ import nacl
 import nacl.utils
 import nacl.secret
 import binascii
-from nacl.public import PrivateKey, Box, PublicKey
 import os
+
+from nacl.public import PrivateKey, Box, PublicKey
+import nacl.hash
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
@@ -81,7 +83,6 @@ def generate_keypair(kB, client_id):
     return pub, priv, encrypted_priv
 
 
-# encrypt big files XXX
 def public_encrypt(message, target_pub, origin_priv):
     priv = PrivateKey(binascii.unhexlify(origin_priv))
     pub = PublicKey(binascii.unhexlify(target_pub))
@@ -95,3 +96,75 @@ def public_decrypt(message, origin_pub, target_priv):
     pub = PublicKey(binascii.unhexlify(origin_pub))
     box = Box(priv, pub)
     return box.decrypt(message)
+
+
+_CHUNK = 1024
+
+# XXX is this the formula?
+_ENC_CHUNK = _CHUNK + Box.NONCE_SIZE + 16
+_HEX_ENC_CHUNK = _ENC_CHUNK * 2
+
+def stream_encrypt(stream, target_pub, origin_priv):
+    """
+    encrypting file process:
+
+    1/ build a box with a random nonce
+    2/ for each 4096 bytes
+        build a hash
+        encrypt
+        send them to the iterator
+
+    3/ at the end create a special control message
+       that is the list of chunk hashes.
+       encrypt it and send it to the iterator
+       this special control message can be used to
+       make sure the encrypted file was not tampered.
+    """
+    priv = PrivateKey(binascii.unhexlify(origin_priv))
+    pub = PublicKey(binascii.unhexlify(target_pub))
+    box = Box(priv, pub)
+    nonce = nacl.utils.random(Box.NONCE_SIZE)
+
+    hashes = []
+
+    while True:
+        data = stream.read(_CHUNK)
+        if not data:
+            break
+        hashes.append(nacl.hash.sha256(data))
+        enc = box.encrypt(data, nonce)
+        yield binascii.hexlify(enc)
+
+    control = ':::'.join(hashes)
+    yield binascii.hexlify(box.encrypt(control, nonce))
+
+
+def stream_decrypt(stream, hash, origin_pub, target_priv):
+    """To decrypt the file
+
+    stream is hex !!
+
+    1/ build a box
+    2/ for each 4096 bytes
+         decrypt
+         build the hash
+         send the decrypted data to the iterator
+    3/ control the hashes send 'OK' or 'KO' to the iterator
+    """
+    priv = PrivateKey(binascii.unhexlify(target_priv))
+    pub = PublicKey(binascii.unhexlify(origin_pub))
+    box = Box(priv, pub)
+
+    hash = binascii.unhexlify(hash)
+    hashes = box.decrypt(hash).split(':::')
+    pos = 0
+
+    while True:
+        data = stream.read(_HEX_ENC_CHUNK)
+        if not data:
+            break
+
+        data = box.decrypt(binascii.unhexlify(data))
+        assert nacl.hash.sha256(data) == hashes[pos]
+        yield data
+        pos += 1
--- fxakeys/tests/test_crypto.py
+++ fxakeys/tests/test_crypto.py
@@ -1,3 +1,4 @@
+from StringIO import StringIO
 import os
 import binascii
 import unittest
@@ -5,6 +6,7 @@ import unittest
 from fxakeys.crypto import encrypt_data, decrypt_data, SymmetricBox
 from fxakeys.crypto import generate_keypair, get_kBr
 from fxakeys.crypto import public_encrypt, public_decrypt
+from fxakeys.crypto import stream_encrypt, stream_decrypt
 
 
 class TestCrypto(unittest.TestCase):
@@ -42,3 +44,21 @@ class TestCrypto(unittest.TestCase):
         message = 'yeah'
         enc = public_encrypt(message, tarek_pub, bob_priv)
         self.assertEqual(public_decrypt(enc, bob_pub, tarek_priv), message)
+
+    def test_stream(self):
+        kB = os.urandom(32)
+        client_id = 'whatever'
+        bob_pub, bob_priv, __ = generate_keypair(kB, client_id)
+
+        kB2 = os.urandom(32)
+        tarek_pub, tarek_priv, __ = generate_keypair(kB, client_id)
+
+        message = '$' * 3000 + 'ok' + 'welp' * 100
+
+        encrypted = list(stream_encrypt(StringIO(message), tarek_pub, bob_priv))
+        enc_data = ''.join(encrypted[:-1])
+        enc_ctrl = encrypted[-1]
+
+        data = stream_decrypt(StringIO(enc_data), enc_ctrl, bob_pub, tarek_priv)
+        data = ''.join(data)
+        self.assertEqual(message, data)
